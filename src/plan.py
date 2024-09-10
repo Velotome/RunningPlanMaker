6ed30212bcf6b8dcc5d4c57f3ed30b6e9ffe70e3
Velotome <noemarzat@gmail.com>
2024-09-10 10:57:13 +0200

    feat : generation of weekly volume according to week type

--- src/plan.py
+++ src/plan.py
@@ -11,7 +11,7 @@ class Plan():
                start_date : datetime.date, 
                race_date : datetime.date, 
                race_km : int, 
-               starting_weekly_km : int = 0):
+               starting_volume : int = 0):
     
     # race specific info
     self.race_km = race_km
@@ -20,6 +20,7 @@ class Plan():
     # plan parameters
     self.nb_taper = 2
     self.build_cycle = 4
+    self.starting_volume = starting_volume
 
     self.cal = calendar.Calendar(firstweekday = 0)
     
@@ -39,38 +40,40 @@ class Plan():
     
     result = ""
     for key, value in self.plan.items():
-      result = result + f"Week {key} :\n{next(iter(value))}\n"
+      result = result + f"{next(iter(value))}\n"
     return result
 
 
 
   def generatePlan(self):
     monday = utils.findMonday(self.race_date)
-    race_week = self.duration_weeks + 1
+    race_week = self.duration_weeks 
+    volume = self.starting_volume
     for week in range(1, race_week):
       match week :
         # Race week
-        case 0 if week == race_week:
-          self.plan[week] = {Week(utils.findMonday(self.race_date), utils.WeekType.RACE)}
+        case week if week == race_week-1:
+          self.plan[week] = {Week(week, utils.findMonday(self.race_date), utils.WeekType.RACE, volume = 0.0)}
         # Taper week
-        case 1 if week > race_week - self.nb_taper:
-          self.plan[week] = {Week(monday, utils.WeekType.TAPER)}
+        case week if week >= race_week-1 - self.nb_taper:
+          self.plan[week] = {Week(week, monday, utils.WeekType.TAPER, 0.5 * volume)}
         # Reduced week
-        case 2 if week % self.build_cycle == 0:
-          self.plan[week] = {Week(monday, utils.WeekType.REDUCED)}
+        case week if week % self.build_cycle == 0:
+          self.plan[week] = {Week(week, monday, utils.WeekType.REDUCED, 0.5 * volume)}
         # Build week
         case _ :
-          self.plan[week] = {Week(monday, utils.WeekType.BUILD)}
+          volume = round(1.1 * volume, 1)
+          self.plan[week] = {Week(week, monday, utils.WeekType.BUILD, volume)}
       monday = monday - datetime.timedelta(7)
 
 if __name__ == "__main__":
   start_date = datetime.date(2024, 9, 9)
-  race_date = datetime.date(2024, 9, 9) + datetime.timedelta(21)
+  race_date = datetime.date(2024, 9, 9) + datetime.timedelta(84)
 
   test_plan = Plan(start_date=start_date,
                    race_date=race_date,
                    race_km=100,
-                   starting_weekly_km=50)
+                   starting_volume=10)
   
   test_plan.generatePlan()
 
--- src/week.py
+++ src/week.py
@@ -3,39 +3,41 @@ import calendar
 
 from day import Day
 from workout import Workout
-from utils import WeekType, WorkoutType
+import utils
 
 class Week : 
 
-  def __init__(self, first_date : datetime.date, week_type : WeekType):
-    self.week_type = week_type
+  def __init__(self, id : int, first_date : datetime.date, type : utils.WeekType, volume : float = 0.0):
+    self.id = id
+    self.type = type
+    self.volume = volume
 
     self.days = {}
     for day in calendar.Calendar().iterweekdays():
       date = first_date + datetime.timedelta(day)
       match day :
         case 0:
-          self.days[calendar.MONDAY] = {Day(date, Workout(WorkoutType.REST))}
+          self.days[calendar.MONDAY] = {Day(date, Workout(utils.WorkoutType.REST))}
         case 1:
-          self.days[calendar.TUESDAY] = {Day(date, Workout(WorkoutType.SPEED))}
+          self.days[calendar.TUESDAY] = {Day(date, Workout(utils.WorkoutType.SPEED))}
         case 2:
-          self.days[calendar.WEDNESDAY] = {Day(date, Workout(WorkoutType.BASE))}
+          self.days[calendar.WEDNESDAY] = {Day(date, Workout(utils.WorkoutType.BASE))}
         case 3:
-          self.days[calendar.THURSDAY] = {Day(date, Workout(WorkoutType.INTERVALS))}
+          self.days[calendar.THURSDAY] = {Day(date, Workout(utils.WorkoutType.INTERVALS))}
         case 4:
-          self.days[calendar.FRIDAY] = {Day(date, Workout(WorkoutType.RECOVERY_RUN))}
+          self.days[calendar.FRIDAY] = {Day(date, Workout(utils.WorkoutType.RECOVERY_RUN))}
         case 5:
-          self.days[calendar.SATURDAY] = {Day(date, Workout(WorkoutType.LONG_RUN))}
+          self.days[calendar.SATURDAY] = {Day(date, Workout(utils.WorkoutType.LONG_RUN))}
         case 6:
-          self.days[calendar.SUNDAY] = {Day(date, Workout(WorkoutType.LONG_RUN))}
+          self.days[calendar.SUNDAY] = {Day(date, Workout(utils.WorkoutType.LONG_RUN))}
   
   def __str__(self) :
-    result = ""
+    result = f"Week {self.id} - {self.type.name} : {self.volume}km\n"
     for value in self.days.values():
       result = result + str(next(iter(value)))
     return result
         
 if __name__ == "__main__":
   test_date = datetime.date(2024, 9, 9)
-  test_week = Week(test_date, week_type = WeekType.BUILD)
+  test_week = Week(1, test_date, utils.WeekType.BUILD, 50.0)
   print(test_week)
